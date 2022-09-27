dc2773d12f623a7c1c0dac235a32c8e190b24e32
Christian Lessa <christianclessa@gmail.com>
2022-09-27 19:45:40 -0300

    Requisito 2 - função test_relatorio_produto - concluido

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,23 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "farinha",
+        "Farinini",
+        "01-05-2021",
+        "02-06-2023",
+        "1234567891",
+        "instrucao1",
+    )
+
+    result = (
+        f"O produto {product.nome_do_produto}"
+        f" fabricado em {product.data_de_fabricacao}"
+        f" por {product.nome_da_empresa} com validade"
+        f" até {product.data_de_validade}"
+        f" precisa ser armazenado {product.instrucoes_de_armazenamento}."
+    )
+
+    assert product.__repr__() == result
