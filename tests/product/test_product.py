e6bb36f2cea94447b3245d05c98a0e03c34b7c3c
Christian Lessa <christianclessa@gmail.com>
2022-09-27 18:54:49 -0300

    Requisito 1 - função test_cria_produtos - concluido

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        id=1,
+        nome_do_produto="Camera",
+        nome_da_empresa="Amazon",
+        data_de_fabricacao="2022-01-10",
+        data_de_validade="2023-01-10",
+        numero_de_serie="1234567891",
+        instrucoes_de_armazenamento="instrucao1",
+    )
+
+    assert product.id == 1
+    assert product.nome_do_produto == "Camera"
+    assert product.nome_da_empresa == "Amazon"
+    assert product.data_de_fabricacao == "2022-01-10"
+    assert product.data_de_validade == "2023-01-10"
+    assert product.numero_de_serie == "1234567891"
+    assert product.instrucoes_de_armazenamento == "instrucao1"
