f01eca548b5fc780ac2a0537f7d5e4327afba7df
Christian Lessa <christianclessa@gmail.com>
2022-09-28 01:55:22 -0300

    Requisito 7 - extraindo dados de arquivo ".xml"

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -2,9 +2,23 @@ from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 import csv
 import json
+from xml.etree import ElementTree
 
 
 class Inventory:
+
+    @classmethod
+    def extract_data_xml(clas, path):
+        with open(path) as files:
+            root_xml = ElementTree.parse(files).getroot()
+            file_xml = []
+            for index in root_xml:
+                products = {}
+                for i in index:
+                    products[i.tag] = i.text
+                file_xml.append(products)
+            return file_xml
+
     @classmethod
     def extract_data(clas, path):
         data = []
@@ -16,6 +30,9 @@ class Inventory:
         elif path.endswith(".json"):
             with open(path) as json_files:
                 data = json.load(json_files)
+        elif path.endswith(".xml"):
+            data = Inventory.extract_data_xml(path)
+
         return data
 
     @classmethod
